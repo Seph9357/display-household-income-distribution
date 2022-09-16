db4ab833a5f48191b68e606d702a85ec0321de5e
Seph9357 <wangziyang19930507@gmail.com>
2022-09-16 21:32:08 +1000

    create new state income info table

--- service/models.py
+++ service/models.py
@@ -14,3 +14,21 @@ class Sa4StateConfig(models.Model):
         verbose_name = 'sa4_state_info'
         verbose_name_plural = verbose_name
 
+
+class StateIncomeCounts(models.Model):
+    """table stores household counts broken down by State, Household Composition and
+       Weekly Household Income Bracket
+    """
+    state_id = models.IntegerField("state_id", null=False, blank=False, db_index=True)
+    state_name = models.CharField('state_name', max_length=64, null=False, blank=False, db_index=True)
+    household_composition = models.CharField('household_composition', max_length=64, null=False, blank=False, db_index=True)
+    weekly_household_income = models.CharField('weekly_household_income', max_length=64, null=False, blank=False, db_index=True)
+    count = models.IntegerField('count', null=True, blank=True)
+
+    class Meta:
+        db_table = 'counts_state_income'
+        verbose_name = 'state_income_distribution'
+        verbose_name_plural = verbose_name
+
+
+
